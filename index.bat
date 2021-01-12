8232599af85fa86094b5d6337617cae030a0887f
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:13:41 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/jwt_auth/*.java&&java com/krzem/jwt_auth/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/jwt_auth/Main.java&&jar cvmf ../manifest.mf ../build/jwt_auth.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="jwt_auth.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/jwt_auth.jar
+:end
