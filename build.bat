7d93e76f5b58cd185ffdcea5795c7232f5a697a6
Krzem <krzem5.dev@gmail.com>
2021-04-10 10:00:21 +0200

    Push Update 04/10/2021, 09:59:53

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/jwt_auth/Main.java&&jar cvmf ../manifest.mf ../build/jwt_auth.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="jwt_auth.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/jwt_auth.jar
-:end
+python build.py %*
